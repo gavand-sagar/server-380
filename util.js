6a52d6d91461a8a988d9d4ddece9553d7638bbcf
gavand-sagar <gavand.sagar.s@gmail.com>
2024-06-18 20:28:29 +0530

    changed db

--- util.js
+++ util.js
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
 
 export async function getDB() {
-    let client = new MongoClient("mongodb://127.0.0.1:27017/")
+    let client = new MongoClient("mongodb+srv://admin:123@cluster0.nva7yak.mongodb.net/")
     let connection = await client.connect();
     let db = connection.db("superman");
     return db;
